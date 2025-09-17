1ee640349a32839d326bb0ecccd3d941bf068677
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2025-09-17 20:13:17 +0700

    First commit

---
+++ FE_Odata/FE-Odata-app/src/App.tsx
@@ -0,0 +1,12 @@
+import CovidDashboard from "./pages/CovidDashboard";
+
+function App() {
+  return (
+    <div>
+      <h1>Demo Temp Data</h1>
+      <CovidDashboard />
+    </div>
+  );
+}
+
+export default App;
