89dd60408d733c7bb158d9785eb920d51677f26d
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2025-09-20 16:22:37 +0700

    Add time line chart

--- FE_Odata/src/App.tsx
+++ FE_Odata/src/App.tsx
@@ -1,26 +1,31 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CovidDashboard from "./pages/CovidDashboard";
 import CovidMapTabs from "./pages/CovidMapTabs";
+import CovidChart from "./pages/CovidChart";
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>Demo Temp Data</h1>
+        <h1>Demo OData</h1>
 
         {/* Nút chuyển trang */}
         <nav style={{ marginBottom: "16px" }}>
           <Link to="/dashboard" style={{ marginRight: "8px" }}>
             <button>Dashboard</button>
           </Link>
-          <Link to="/map">
+          <Link to="/map" style={{ marginRight: "8px" }}>
             <button>Map</button>
+          </Link>
+          <Link to="/chart">
+            <button>Daily Report</button>
           </Link>
         </nav>
 
         {/* Định tuyến */}
         <Routes>
           <Route path="/dashboard" element={<CovidDashboard />} />
+          <Route path="/chart" element={<CovidChart />} />
           <Route path="/map" element={<CovidMapTabs />} />
         </Routes>
       </div>
